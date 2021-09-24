9c901be935a49d89a8d9a81b108c4cfe901e0ff2
moons <pooh2314@naver.com>
2021-09-24 21:48:43 +0900

    SpringBoot Test를 위한 라이브러리 추가

--- buildSrc/src/main/kotlin/common-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/common-conventions.gradle.kts
@@ -26,8 +26,14 @@ dependencies {
         implementation("org.apache.commons:commons-text:1.9")
     }
 
-    // Use JUnit Jupiter for testing.
-    testImplementation("org.junit.jupiter:junit-jupiter:5.7.2")
+    // JUnit 5 = JUnit Platform + JUnit Jupiter + JUnit Vintage
+    // JUnit Platform : 테스트 프레임워크가 동작하도록 TestEngine API를 정의하는 모듈
+    // JUnit Jupiter : JUnit 5에서 지원하는 새로운 기능들을 위한 모듈
+    // JUnit Vintage : JUnit 5 플랫폼에서 JUnit 3나 JUnit 4 기반의 테스트를 지원하기 위한 모듈
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        // 5버전만 사용하자. 헤깔리니까. JUnit 4 모듈은 제외하도록하자.
+        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+    }
 }
 
 tasks.test {
